e36e8dd9bca67ce9199b9f797950c8422dd6f3bf
Victor Hugo <victormagalhaes00@outlook.com>
2021-04-29 00:02:54 -0300

    [DEV] Adding utils functions

---
+++ src/api/Utils/index.js
@@ -0,0 +1,18 @@
+import constants from "../Constants/index.js";
+
+export const checkCommonRealtyValidity = (usableAreas, lon, lat) => {
+  return usableAreas != 0 && lon != 0 && lat != 0;
+};
+
+export const isRealtyInsideZapBoundingBox = (lon, lat) => {
+  return (
+    lat < constants.boundingBox.maxlat &&
+    lat > constants.boundingBox.minlat &&
+    lon < constants.boundingBox.maxlon &&
+    lon > constants.boundingBox.minlon
+  );
+};
+
+export const paginate = (filteredRealty, page, pageSize) => {
+  return filteredRealty.slice((page - 1) * pageSize, page * pageSize);
+};
